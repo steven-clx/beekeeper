ace3416fcf27e82260dbc7ba599b8b3cc0e9c0ce
Steven <development@jumbea.com>
2020-07-02 00:42:01 +0800

    initial commit

---
+++ src/main/java/com/steven/beekeeper/BeekeeperApplication.java
@@ -0,0 +1,13 @@
+package com.steven.beekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeekeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeekeeperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/steven/beekeeper/BeekeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steven.beekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeekeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
